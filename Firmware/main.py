fc481250d8ed012e6f1b701e160b38ec7a0a3f0f
Elnur Mavlonov <250077@newuu.uz>
2026-06-06 17:28:03 +0500

    feat: implement initial Macropad firmware functionality

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,3 +1,21 @@
-# Macropad Firmware (Placeholder)
-# To be implemented.
-print("Macropad starting...")
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.D9, board.D10, board.D11)
+keyboard.row_pins = (board.D12)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [
+        KC.LCMD(KC.C),
+        KC.LCMD(KC.V), 
+        KC.MUTE,
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
